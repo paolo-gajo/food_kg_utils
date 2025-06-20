4454ecfbd25315c462e9db031d843ecad58b8b51
paolo-gajo <paolo.gajo2@unibo.it>
2025-06-20 19:25:17 -0300

    bert ner wip

--- src/bert_ner.py
+++ src/bert_ner.py
@@ -1,6 +1,23 @@
 # Load model directly
 from transformers import AutoTokenizer, AutoModelForTokenClassification
+import json
 
-tokenizer = AutoTokenizer.from_pretrained("Davlan/bert-base-multilingual-cased-ner-hrl")
-model = AutoModelForTokenClassification.from_pretrained("Davlan/bert-base-multilingual-cased-ner-hrl")
+def main():
+    model_name = "Davlan/bert-base-multilingual-cased-ner-hrl"
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
+    model = AutoModelForTokenClassification.from_pretrained(model_name)
 
+    json_path = './data/gz_graph.json'
+    
+    with open(json_path, 'r', encoding='utf8') as f:
+        data = json.load(f)
+    
+    for line in data:
+        input = tokenizer(data['presentation'], return_tensors='pt')
+        output = model(**input)
+
+        ...
+
+
+if __name__ == "__main__":
+    main()
